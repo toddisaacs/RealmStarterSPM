592482217da4381527f44a01c8056313e2a536ae
Todd Isaacs <teisaacs21w@gmail.com>
2023-05-01 13:11:30 -0400

    Initial Commit

---
+++ RealmStarterSPM/RealmStarterSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmStarterSPMApp.swift
+//  RealmStarterSPM
+//
+//  Created by Todd Isaacs on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmStarterSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
